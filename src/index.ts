96a5405ee5f0a77b06e1a6ac8b97067d94f91f94
nitin443 <imnitin55@gmail.com>
2024-04-01 12:30:32 +0530

    edit

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 import express, { Express, Request, Response , Application } from 'express';
 import dotenv from 'dotenv';
+const cors = require('cors');
+
 
 //For env File 
 dotenv.config();
 
+
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-app.get('/', (req: Request, res: Response) => {
+app.use(cors());
+
+
+app.get('/be', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript -> ****Backend ****Server');
+});
+
+app.get('/be/data', (req: Request, res: Response) => {
+  res.send({'Name': 'Backend', 'Test': 'Sample Data'});
 });
 
 app.listen(port, () => {
